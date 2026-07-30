1e37fcd876a16c73b0e50b40893e26073ab0994c
Riteshchowdary-07 <medasaniritesh2007@gmail.com>
2026-07-31 01:17:59 +0530

    Deploy_LifeLink

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   server: {
     port: 3005,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
